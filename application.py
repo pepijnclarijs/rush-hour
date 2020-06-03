b75c82209a11e1fe0d4d46290db828735851c0be
DarkdealCC <44091669+DarkdealCC@users.noreply.github.com>
2020-06-03 12:43:57 +0200

    Class Game created

--- application.py
+++ application.py
@@ -1,1 +1,12 @@
 # Start solving Rush Hour here
+Class Game():
+    def __init__ (self, board, vehicles):
+        self.board = board
+        self.vehicles = vehicles
+    def description(self):
+        return str(f'{self.board} contains: {self.vehicles}.')
+
+Class Board():
+
+
+Class Vehicles():
