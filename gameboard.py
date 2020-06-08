fe3223af3ff4cc2b6da0b325060b37377b435ca3
pepijn <pepijn@greener.nu>
2020-06-08 16:13:49 +0200

    Added the first worked out implementations of the classes Game, Board and Vehicle with their attributes and methods. Also added main.py in which a game of rush hour is solved 10 times.

--- gameboard.py
+++ gameboard.py
@@ -1,4 +1,17 @@
-# Start solving Rush Hour here
+import math
+
+# TODO: I called stuff x and y but is should all be changed to row and column. Otherwise it would lead to a lot of /
+# 	confusion since a coordinate system names the horizontal axis the x axis and the vertical axis the y axis. /
+# 	Now I use the x axis to indicate the rows which would be logically/visually the y axis in a normal coordinate /
+# 	system.
+
+# TODO: Maybe change the variable name 'exit' because it shadows a built-in function.
+
+# TODO: The code in the get_passed_coor function is now dependent of how the coordinates of a vehicle are implemented.
+# 	It should now be implemented in this way: if horizontal: [(left most coor), (middle coor), (right most coor)].
+# 	If vertical: [(lower coor), (middle coor), (upper coor)]
+
+
 class Game:
     """
     This class represents a game of rush hour.
@@ -7,101 +20,152 @@ class Game:
         board: This is an instance of an empty game board.
         vehicles: This is a list containing the vehicles present in the game.
     """
-    def __init__ (self, board, vehicles):
+
+    def __init__(self, board, vehicles):
         self.board = board
         self.vehicles = vehicles
 
+        # Remember the taken boxes on the board.
+        self.taken_boxes = []
+        for vehicle in self.vehicles:
+            self.taken_boxes.extend(vehicle.get_position())
+
+    def get_board(self):
+        return self.board
+
+    def get_vehicles(self):
+        return self.vehicles
+
+    def get_taken_boxes(self):
+        return self.taken_boxes
+
+    def update_taken_boxes(self):
+        taken_positions = []
+        for vehicle in self.vehicles:
+            taken_positions.extend(vehicle.get_position())
+        self.taken_boxes = taken_positions
+
     def description(self):
-        return str(f'{self.board} contains: {self.vehicles}.')
+        return f"{self.board} contains: {self.vehicles}."
 
-    def finished(self):
+    def is_finished(self):
         """
-        Returns:
-            Boolean Indicating whether the game is won or not.
-        """
-        # TODO: write this function
+		Returns:
+			Boolean Indicating whether the game is won or not.
+		"""
 
-    def move(vehicle, steps):
+        # Loop over all vehicles.
+        # TODO: find a more efficient way to get access to the red car.
+        for vehicle in self.get_vehicles():
+            # Find the red car.
+            if vehicle.get_name() == 'X':
+                red_car = vehicle
+
+        # Get the box that is in front of the exit.
+        exit = self.get_board().get_exit()
+
+        # Get the boxes between the exit and the red car.
+        box = red_car.get_position()[1]
+        while box != exit:
+            # Get the next spot
+            next_row = box[0]
+            next_column = box[1] + 1
+            next_spot = (next_row, next_column)
+
+            # Loop over all vehicles on the board.
+            for vehicle in self.get_vehicles():
+                # Check if a vehicle is blocking the exit.
+                if next_spot in vehicle.get_position():
+                    # If a vehicle is blocking the exit, the game is not finished.
+                    return False
+
+            box = (next_row, next_column)
+
+            # Check if the exit is reached.
+            if box == exit:
+                # If all spots between the red car and the exit are free, the game is finished.
+                return True
+
+    def is_move_allowed(self, moving_vehicle, end_position):
         """
-        Moves a vehicle by an amount of 'steps'.
+        Checks if a move is allowed by checking whether the passed boxes are not occupied and lie within the board.
 
         Args:
-            vehicle: Vehicle that is moved.
-            steps: Integer representing the number of steps the vehicle should move.
+            end_position: List of tuples indicating the end position of the vehicle.
+            moving_vehicle: The vehicle that is moving to the end position indicated by end_position argument.
 
         Returns:
-             Boolean indicating whether the vehicle was successfully moved or not.
+            Boolean indicating whether the vehicle is allowed to move to the given position.
         """
 
-        # Get the coordinates and orientation of the vehicle.
-        orientation = vehicle.get_orientation()
-        coordinate_pairs = vehicle.get_coordinates()
+        # Get all boxes that are passed when the vehicle moves to its end position.
+        passed_boxes = moving_vehicle.get_passed_boxes(end_position)
 
-        # Loop over the coordinate pairs.
-        new_coordinates = []
-        for coordinates in coordinate_pairs:
-            # Get the x and y coordinates.
-            x_coordinate = coordinates[0]
-            y_coordinate = coordinates[1]
+        # Add the end position itself as well.
+        passed_boxes.extend(end_position)
 
-            # Check which coordinate should change.
-            if orientation == 'x':
-                new_x_coordinate = coordinates[0] + steps
-                # Create the new coordinates of the vehicle.
-                new_coordinates.append([new_x_coordinate, y_coordinate])
-            else:
-                new_y_coordinate = coordinates[1] + steps
-                # Create the new coordinates of the vehicle.
-                new_coordinates.append([x_coordinate, new_y_coordinate])
+        # Get all taken boxes.
+        taken_boxes = self.get_taken_boxes()
+        for box in passed_boxes:
+            # Skip the boxes that are already taken by the current vehicle.
+            if box in moving_vehicle.get_position():
+                continue
+            # Check if the box is taken.
+            if box in taken_boxes:
+                return False
 
-        # Check if the new coordinates are valid.
-        # Pseudo code:
-        # if gameboard.is_move_allowed(coordinates):
-        #   # Set the new coordinates of the vehicle.
-        #   vehicle.set_coordinates(new_coordinates)
-        #   return True
-        # else:
-        #   return False
-        # TODO: write this function
+            # TODO: this could be optimized a little by only checking for the outer most box taken by the vehicle to be
+            #  on the board.
+            # Check if the row and column are inside the board.
+            row = box[0]
+            column = box[1]
+            board = self.get_board()
+            if (row < 1
+                    or row > board.get_length()
+                    or column < 1
+                    or column > board.get_length()):
+                return False
+        return True
 
-        def is_move_allowed(self, coordinate_pairs):
-            """
-            Checks if a move is allowed by checking whether the coordinates are not occupied and lie within the field of the
-            game.
-
-            Args:
-                coordinate_pairs: list of pairs of coordinates indicating the position of the vehicle.
-
-            Returns:
-                 Boolean indicating whether the vehicle is allowed to move to the given position.
-            """
-            # Get all coordinates that are not allowed.
-            # Pseudo code:
-            # # Loop over all vehicles on the board.
-            # for vehicle in self.vehicles:
-            #     # Loop over all coordinates that need to be checked.
-            #     for coordinates in coordinate_pairs:
-            #         # Check if the coordinate is free.
-            #         if coordinates == vehicle.get_coordinates():
-            #             return False
-            #         # Check if x and y coordinate are inside the board.
-            #         x_coor = coordinates[0]
-            #         y_coor = coordinates[1]
-            #         if (x_coor < left_border or x_coor > right_border or y_coor < lower_border or y_coor > upper_border):
-            #             return False
-            # return True
-            pass
 
 class Board:
     """
     This class represents an empty board in a game of rush hour.
-w
+
     Args:
         length: Integer representing the length of the board.
     """
+
     def __init__(self, length):
-        # TODO: create a grid that contains a value that represents an empty spot.
-        pass
+        self.length = length
+
+        # TODO: Maybe make exit a pair of coordinates (indicating the red car is at that position would be easier that way)
+
+        # Find the box of the exit.
+        if length % 2 == 0:
+            self.exit = (int(length / 2), length)
+        else:
+            self.exit = (int(math.ceil(length / 2)), length)
+
+    # TODO: Would it be better to define an upper, lower, right and left border instead of just using the length /
+    #  whenever it is needed to indicate the borders of the board? That would get rid of some magic numbers /
+    #  (the 1 in x_coor < 1 and y_coor < 1) in the is_move allowed function above. However it would cost many /
+    #  lines of code (because of including getters for / these attributes).
+
+    def get_length(self):
+        """
+        Returns:
+            Integer indicating the length of the board.
+        """
+
+        return self.length
+
+    def get_exit(self):
+        """
+        Returns:
+            Tuple of integers indicating the box that is in front of the exit.
+        """
+        return self.exit
 
 
 class Vehicle:
@@ -110,22 +174,202 @@ class Vehicle:
 
     Args:
         name: String representing the name for the vehicle.
-        length: Integer representing the length of the vehicle.
-        coordinates: List containing integers representing the coordinates on the game board that the vehicle occupies.
+        size: Integer representing the length of the vehicle.
+        position: List tuples containing integers. The list represents the position of the vehicle on the board.
+        orientation: String that indicates the direction of the vehicle.
     """
-    def __init__(self, name, size, coordinates):
+
+    def __init__(self, name, size, position, orientation):
         self.name = name
         self.size = size
-        self.coordinates = coordinates
+        self.position = position
+        self.orientation = orientation
+
+    def get_name(self):
+        """
+        Returns:
+            String indicating the name of the vehicle.
+        """
+
+        return self.name
+
+    def get_size(self):
+        """
+        Returns:
+            Integer indicating the size of the vehicle.
+        """
+
+        return self.size
+
+    def get_position(self):
+        """
+        Returns:
+            List tuples containing integers. The list represents the position of the vehicle on the board.
+        """
+
+        return self.position
+
+    def set_position(self, new_position):
+        """
+        Changes the position of the vehicle.
+
+        Args:
+            new_position: List of tuples that hold a position. It indicates the future location of the vehicle.
+        """
+        self.position = new_position
+
+    def get_orientation(self):
+        """
+        Returns:
+            String indicating the orientation of the vehicle.
+        """
+
+        return self.orientation
 
     def description(self):
         """
         Returns:
-             String containing the name, size and coordinates of the vehicle.
+            String containing the name, size and position of the vehicle.
         """
-        # TODO: create this function
+        return f"Vehicle {self.get_name()} of size {self.get_size} at position {self.get_position()} with " \
+               f"orientation {self.get_orientation()}"
 
+    def speculate_new_position(self, steps):
+        """
+        Speculates the new position of the vehicle after moving by a number of steps.
 
+        Args:
+            steps: Integer representing the number of steps the vehicle would move.
 
+        Returns:
+            List of tuples indicating the would be new position of the vehicle after moving by a number of steps.
+        """
 
+        # Get the position and orientation of the vehicle.
+        orientation = self.get_orientation()
+        position = self.get_position()
 
+        # Loop over the position.
+        new_position = []
+        for box in position:
+            # Get the row and column.
+            row = box[0]
+            column = box[1]
+
+            # Check which one should be changed.
+            if orientation == 'horizontal':
+                new_column = box[1] + steps
+                # Create the new position of the vehicle.
+                new_position.append((row, new_column))
+            else:
+                new_row = box[0] + steps
+                # Create the new position of the vehicle.
+                new_position.append((new_row, column))
+
+        return new_position
+
+    def get_passed_boxes(self, end_position):
+        """
+        Gets the boxes that are passed when a vehicle moves from its place to the 'end_box' box.
+
+        Args:
+            end_position: Tuple representing the position up until which the passed boxes should be found.
+
+        Returns:
+            List of tuples that represent the boxes between the vehicle and the given end position.
+        """
+
+        # TODO: use the name 'start_position' instead of 'vehicle_position'.
+
+        # Get the position, orientation and size of the vehicle.
+        orientation = self.get_orientation()
+        vehicle_position = self.get_position()
+
+        # TODO: make the below a little cleaner.
+        # Make place for the enclosed boxes.
+        enclosed_boxes = []
+
+        # Check the orientation of the vehicle.
+        if orientation == 'horizontal':
+            # Sort the boxes representing the vehicle position on column value.
+            sorted_boxes = sorted(vehicle_position, key=lambda pair: pair[1])
+
+            # Get the right most box.
+            vehicle_right_most_box = sorted_boxes[-1]
+
+            # Get the left most box.
+            vehicle_left_most_box = sorted_boxes[0]
+
+            # Sort the boxes in the end position based on column values from small to large.
+            sorted_end_position = sorted(end_position, key=lambda pair: pair[1])
+
+            # Get the left most and right most box of the end position.
+            end_position_left_most_box = sorted_end_position[0]
+            end_position_right_most_box = sorted_end_position[-1]
+
+            # Check if the end position is to the right of the vehicle.
+            if end_position_left_most_box[1] >= vehicle_right_most_box[1]:
+                # Get the number of boxes between the vehicle and its end position.
+                enclosed_boxes_count = end_position_left_most_box[1] - vehicle_right_most_box[1]
+
+                # Get the enclosed boxes. # TODO: put this in a helper function.
+                for i in range(1, enclosed_boxes_count):
+                    enclosed_box = (vehicle_right_most_box[0], vehicle_right_most_box[1] + i)
+                    enclosed_boxes.append(enclosed_box)
+            else:
+                # Get the number of boxes between the vehicle and its end position.
+                enclosed_boxes_count = vehicle_left_most_box[1] - end_position_right_most_box[1]
+
+                # Get the enclosed boxes. # TODO: put this in a helper function.
+                for i in range(1, enclosed_boxes_count):
+                    enclosed_box = (vehicle_left_most_box[0], vehicle_left_most_box[1] - i)
+                    enclosed_boxes.append(enclosed_box)
+        else:
+            # Sort the boxes representing the vehicle position on row value.
+            sorted_boxes = sorted(vehicle_position, key=lambda pair: pair[0])
+
+            # Get the lower most box of the vehicle.
+            vehicle_lower_most_box = sorted_boxes[-1]
+
+            # Get the upper most box of the vehicle.
+            vehicle_upper_most_box = sorted_boxes[0]
+
+            # Sort the boxes in the end position based on row values from small to large.
+            sorted_end_position = sorted(end_position, key=lambda pair: pair[0])
+
+            # Get the upper and lower most box of the end position.
+            end_position_upper_most_box = sorted_end_position[0]
+            end_position_lower_most_box = sorted_end_position[-1]
+
+            # Check if the end position is above the vehicle.
+            if end_position_lower_most_box[0] <= vehicle_upper_most_box[0]:
+                enclosed_boxes_count = vehicle_upper_most_box[0] - end_position_lower_most_box[0]
+
+                # Get the enclosed boxes. # TODO: put this in a helper function.
+                for i in range(1, enclosed_boxes_count):
+                    enclosed_box = (vehicle_upper_most_box[0] - i, vehicle_upper_most_box[1])
+                    enclosed_boxes.append(enclosed_box)
+            else:
+                enclosed_boxes_count = end_position_upper_most_box[0] - vehicle_lower_most_box[0]
+
+                # Get the enclosed boxes. # TODO: put this in a helper function.
+                for i in range(1, enclosed_boxes_count):
+                    enclosed_box = (vehicle_lower_most_box[0] + i, vehicle_lower_most_box[1])
+                    enclosed_boxes.append(enclosed_box)
+
+        return enclosed_boxes
+
+    def move(self, new_position):
+        """
+        Moves a vehicle by a number of 'steps'.
+
+        Args:
+            new_position: List of tuples indicating the position of the vehicle.
+
+        Returns:
+            None
+        """
+
+        # TODO: This function is not necessary I think. set_position does exactly the same right?
+
+        self.set_position(new_position)
--- main.py
+++ main.py
@@ -1,1 +1,82 @@
-from gameboard.py import GameBoard, Vehicle
+from gameboard import Game, Board, Vehicle
+import random
+
+# TODO: 1) Create a list of vehicles with the horizontal coordinates.
+#   2) Make an instance of the game class.
+#   3) Write the finished function in the game class
+#   4) Move vehicles around and save the movement in the excel format (car, move)
+#   5) When the win condition is met, stop moving and print the list of moves.
+
+# Solve the game 10 times.
+solved_cases = {}
+for i in range(10):
+    # Create the 6x6 game board.
+    board = Board(6)
+
+    # Create the begin situation.
+    A = Vehicle('A', 2, [(1, 2), (1, 3)], 'horizontal')
+    B = Vehicle('B', 3, [(1, 4), (1, 5), (1, 6)], 'horizontal')
+    C = Vehicle('C', 2, [(2, 2), (2, 3)], 'horizontal')
+    D = Vehicle('D', 2, [(2, 4), (3, 4)], 'vertical')
+    E = Vehicle('E', 2, [(2, 5), (2, 6)], 'horizontal')
+    X = Vehicle('X', 2, [(3, 1), (3, 2)], 'horizontal')
+    F = Vehicle('F', 2, [(3, 3), (4, 3)], 'vertical')
+    G = Vehicle('G', 2, [(4, 1), (4, 2)], 'horizontal')
+    H = Vehicle('H', 2, [(4, 4), (4, 5)], 'horizontal')
+    I = Vehicle('I', 2, [(3, 6), (4, 6)], 'vertical')
+    J = Vehicle('J', 2, [(5, 1), (6, 1)], 'vertical')
+    K = Vehicle('K', 2, [(5, 3), (6, 3)], 'vertical')
+    L = Vehicle('L', 2, [(5, 5), (5, 6)], 'horizontal')
+
+    vehicles = [A, B, C, D, E, F, G, H, I, J, K, L, X]
+
+    # Create the game.
+    game = Game(board, vehicles)
+
+    # Create a list to track the movements.
+    moves = []
+
+    # Move vehicles around randomly until the game is finished.
+    j = 0
+    while not game.is_finished():
+        # Get a random number of steps.
+        steps = 0
+        while steps == 0:
+            steps = random.randint(-4, 4)
+
+        # Get a random vehicle.
+        random_vehicle = vehicles[random.randint(0, 12)]
+
+        # Speculate the new position passed by the vehicle.
+        new_coordinates = random_vehicle.speculate_new_position(steps)
+
+        # Check if the new position is free.
+        test = game.is_move_allowed(random_vehicle, new_coordinates)
+        if test:
+            # Move the vehicle to the new position.
+            random_vehicle.move(new_coordinates)
+            game.update_taken_boxes()
+        else:
+            continue
+
+        # Save the movements of the vehicles.
+        move = (random_vehicle.get_name(), steps)
+        moves.append(move)
+
+        j += 1
+        if j > 1000:
+            moves = "oops! Something went wrong."
+            break
+
+        solved_cases[f"{i}"] = moves
+
+first_iter = True
+previous_case = None
+for case in solved_cases:
+    if first_iter:
+        previous_case = case
+        first_iter = False
+    if len(solved_cases[case]) < len(solved_cases[previous_case]):
+        previous_case = case
+print(f"The case with the least number of moves is case {previous_case} with {len(solved_cases[previous_case])} moves.")
+print(f"The moves are: {solved_cases[previous_case]}")
